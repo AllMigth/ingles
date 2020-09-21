818e4b9fb97b6a4e2b901cf486742856a6e6bdc6
Juan <jgarzon292@gmail.com>
2020-09-20 19:09:55 -0500

    adding google signing

---
+++ app/src/main/java/com/simulador/ingles/Profile.java
@@ -0,0 +1,44 @@
+package com.simulador.ingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Profile extends AppCompatActivity {
+    TextView name, email;
+    Button logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logoutButton);
+        name = findViewById(R.id.logoutButton);
+        email = findViewById(R.id.logoutButton);
+
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            name.setText(signInAccount.getDisplayName());
+            email.setText(signInAccount.getDisplayName());
+        }
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), login_first.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
