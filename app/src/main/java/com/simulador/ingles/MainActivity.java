a85d4449ac1b3c184e57f6c39350b94b2a266ddb
Juan <jgarzon292@gmail.com>
2020-09-21 23:09:07 -0500

    adding glide for request a profile pic from the user account

--- app/src/main/java/com/simulador/ingles/MainActivity.java
+++ app/src/main/java/com/simulador/ingles/MainActivity.java
@@ -1,14 +1,15 @@
 package com.simulador.ingles;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.analytics.FirebaseAnalytics;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
     TextView name, email;
     Button logout;
+    private ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.nameTV);
         email = findViewById(R.id.emailTV);
         btnModulo1 = (Button)findViewById(R.id.btnModulo1);
+        profilePic = findViewById(R.id.profilePicUser);
 
         btnModulo1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         if(signInAccount != null){
             name.setText(signInAccount.getDisplayName());
             email.setText(signInAccount.getEmail());
+            Glide.with(this).load(signInAccount.getPhotoUrl()).into(profilePic);
         }
 
 
